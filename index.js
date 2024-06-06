4db55b12dbb72e7e4815fa7564fcb5e517fe5368
ali.ghavi <aliqavi@outlook.com>
2024-06-06 22:49:33 +0330

    feature: added short month format

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+const monthMap = {
+    'jan': 0,
+    'feb': 1,
+    'mar': 2,
+    'apr': 3,
+    'may': 4,
+    'jun': 5,
+    'jul': 6,
+    'aug': 7,
+    'sep': 8,
+    'oct': 9,
+    'nov': 10,
+    'dec': 11
+};
+
 function runGitCommand(command) {
     try {
         execSync(command, { stdio: 'inherit', shell: 'cmd.exe' });
@@ -15,31 +30,41 @@ function runGitCommand(command) {
     }
 }
 
-function createDummyCommits(date, numCommits) {
+function createDummyCommits(date, numCommits, email) {
     for (let i = 0; i < numCommits; i++) {
         const filename = `dummy_${Date.now()}.txt`;
         fs.writeFileSync(filename, `Dummy commit for ${date}`);
 
         runGitCommand(`git add ${filename}`);
-        runGitCommand(`set "GIT_COMMITTER_DATE=${date}" && git commit --date="${date}" -m "Dummy commit for ${date}"`);
+        runGitCommand(`set "GIT_COMMITTER_DATE=${date}" && git commit --author="Your Name <${email}>" --date="${date}" -m "Dummy commit for ${date}"`);
 
         fs.unlinkSync(filename);
     }
 }
 
-rl.question('Enter the start date (YYYY-MM-DD): ', (startDateStr) => {
-    rl.question('Enter the end date (YYYY-MM-DD): ', (endDateStr) => {
-        const startDate = new Date(startDateStr);
-        const endDate = new Date(endDateStr);
-        let currentDate = startDate;
+function parseDateInput(input) {
+    const [day, monthStr, year] = input.split(' ');
+    const month = monthMap[monthStr.toLowerCase()];
+    return new Date(year, month, day);
+}
 
-        while (currentDate <= endDate) {
-            const dateStr = currentDate.toISOString();
-            const numCommits = Math.floor(Math.random() * 10);
-            createDummyCommits(dateStr, numCommits);
-            currentDate.setDate(currentDate.getDate() + 1);
-        }
+rl.question('Enter your GitHub email: ', (email) => {
+    rl.question('Enter the start date (DD MMM YYYY): ', (startDateStr) => {
+        rl.question('Enter the end date (DD MMM YYYY): ', (endDateStr) => {
+            const startDate = parseDateInput(startDateStr);
+            const endDate = parseDateInput(endDateStr);
+            let currentDate = startDate;
 
-        rl.close();
+            while (currentDate <= endDate) {
+                const dateStr = currentDate.toISOString();
+                const numCommits = Math.floor(Math.random() * 10);
+                createDummyCommits(dateStr, numCommits, email);
+                currentDate.setDate(currentDate.getDate() + 1);
+            }
+
+            rl.close();
+
+            runGitCommand('git push origin main');
+        });
     });
 });
